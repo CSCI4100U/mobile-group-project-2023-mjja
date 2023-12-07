36ac1bba2d37853533508ff66ca82891ae594174
Jahanvi Mathukia <jahanvipareshkumar.mathukia@ontariotechu.net>
2023-12-07 13:38:16 -0500

    Added references in main.dart

--- money_minder/lib/main.dart
+++ money_minder/lib/main.dart
@@ -4,7 +4,11 @@
 //The code in this project was developed with the assistance of an AI tool (cited below)
 //OpenAI. (2023). ChatGPT [Large language model]. https://chat.openai.com
 // https://github.com/Coding-Orbit/awesome_notification/blob/main/android/app/src/main/AndroidManifest.xml
-// Used the above link to help build awesome package notifications
+// https://docs.flutter.dev/
+// https://firebase.google.com/docs/cli#setup_update_cli
+// https://pub.dev/packages/fl_chart
+// https://github.com/imaNNeo/fl_chart/blob/master/repo_files/documentations/pie_chart.md
+// Used the above links to help build this whole application
 
 
 import 'package:flutter/material.dart';
